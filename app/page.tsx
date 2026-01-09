4116d7ee1b3d917ac05c2dca9633792b18227f3a
Romain Goncalves <goncalves.romain@gmail.com>
2026-01-09 11:48:18 +0100

    Proper columns

--- app/page.tsx
+++ app/page.tsx
@@ -1007,7 +1007,7 @@ const EvoSwShowcase = () => {
           </div>
         </div>
 
-        <div className="grid lg:grid-cols-2 gap-12 items-start">
+        <div className="grid grid-cols-1 xl:grid-cols-2 gap-12 items-start">
           <div className="space-y-6">
             {/* <div className="p-6 bg-slate-900 rounded-xl border border-sw/30 mb-6">
               <p className="text-sw text-xs font-mono uppercase tracking-widest mb-2">
